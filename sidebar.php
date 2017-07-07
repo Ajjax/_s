d6df9ba80cebdd670ee2714b5fcdfac6674dd2fb
LBarault <loic.barault@laposte.net>
2017-07-07 15:44:34 +0200

    Add Title of widget and Title on end of sidebar on Sidebar

--- sidebar.php
+++ sidebar.php
@@ -13,5 +13,10 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<?php
+	echo "<h2>Bonjour ceci est une sidebar</h2>";
+	?>
+	<?php dynamic_sidebar( 'sidebar-1' );
+	echo "<h1>Ceci est la fin du widget</h1>";
+	 ?>
 </aside><!-- #secondary -->
